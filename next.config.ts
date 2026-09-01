2f3ffe6ebb7d82e3491de34ea1933d39f61469d1
Guigas Programador <118854055+guigasprogramador@users.noreply.github.com>
2026-08-31 22:04:59 -0300

    fix: remove output standalone incompativel com Vercel

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
-  output: 'standalone',
   experimental: {
     serverActions: {
       bodySizeLimit: '2mb'
